c6feb2439af1132ffbaa541c6ee09c37a92ab2e1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-31 10:46:14 +0100

    wrote function getMoney

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,7 @@
+// Get Money
+function getAmount() {
+  let amount = document.querySelector("#money").value;
+  return amount;
+}
 // Function to calculate the tip
 const calculateTip = (money, rate) => (money * rate).toFixed(2);
