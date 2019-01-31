19652f6b0fb6af989d5ab21ec1b7463b6947f98d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-31 10:52:50 +0100

    wrote new function getRate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
 // Get Money
 function getAmount() {
-  let amount = document.querySelector("#money").value;
+  const amount = document.querySelector("#money").value;
   return amount;
+}
+// Get Rate
+function getRate() {
+  const quality = document.querySelector("#rate").value;
+  switch (quality) {
+    case "excellent":
+      return 0.3;
+      break;
+    case "verygood":
+      return 0.2;
+      break;
+    case "good":
+      return 0.15;
+      break;
+    case "ok":
+      return 0.1;
+      break;
+    case "bad":
+      return 0.05;
+      break;
+    case "very bad":
+      return 0;
+      break;
+    default:
+      throw new Error("You need to choose a quality of service");
+  }
 }
 // Function to calculate the tip
 const calculateTip = (money, rate) => (money * rate).toFixed(2);
